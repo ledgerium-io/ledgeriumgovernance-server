a98ef5ca4f8569b6903f1f9a2cbca7dabcf8d1ba
Ashwath S <ashwath.s@ledgerium.net>
2018-10-24 22:30:26 +1100

    Added the app folder with UI

---
+++ app/governanceUI.js
@@ -0,0 +1,430 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var util = require('util');
+var Web3 = require('web3');
+var moment = require('moment');
+var Promise = require('promise');
+// var storage = require('azure-storage');
+var appjson = require('./version.json')
+
+/*
+ * Parameters
+ */
+var listenPort = process.argv[2] || "3002";
+var consortiumId = process.argv[3] || "111";
+process.env['AZURE_STORAGE_ACCOUNT'] = process.argv[4] || "dontcare";
+process.env['AZURE_STORAGE_ACCESS_KEY'] = process.argv[5] || "dontcare";
+var containerName = process.argv[6] || "dontcare";
+var identityBlobPrefix = process.argv[7] || "passphrase-";
+var ethRpcPort = process.argv[8] || "8545";
+var validatorListBlobName = process.argv[9] || "AddressList.json";
+var paritySpecBlobName = process.argv[10] || "spec.json";
+var valSetContractBlobName = process.argv[11] || "../contracts/SimpleValidatorSet.sol";
+var adminContractBlobName = process.argv[12] || "../contracts/AdminValidatorSet.sol";
+var adminContractABIBlobName = process.argv[13] || "../contracts/AdminValidatorSet.sol.abi";
+var logFilePath=process.argv[14] || "log1.txt";
+
+/*
+ * Constants
+ */
+const refreshInterval = 10000;
+const nodeRegexExp = /enode:\/\/\w{128}\@(\d+.\d+.\d+.\d+)\:\d+$/;
+// var blobService = storage.createBlobService();
+const recentBlockDecrement = 10; // To find a recent block for "/networkInfo", take the "currentBlock - recentBlockDecrement"
+
+var app = express();
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: {
+      json: function(context) {
+        return JSON.stringify(context);
+      }
+  },
+ }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(express.static('share'));
+app.use('/assets', express.static('assets'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var activeNodes = [];
+var abiContent = '';
+var timeStamp;
+var addressList = undefined;
+
+process.on('uncaughtException', err => {
+  if (err.message.includes("ECONNRESET")) {
+    console.log(err);
+  } else throw err;
+});
+process.on('unhandledRejection', err => {
+  if (err.message.includes("ECONNRESET")) {
+    console.log(err);
+  } else throw err;
+});
+
+// Set logging
+var log_file = fs.createWriteStream(logFilePath, {
+  flags: 'a'
+});
+var log_stdout = process.stdout;
+
+console.log = function (d) {
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+/*
+ * Output Parameters to log file
+ */
+console.log("etheradmin.js starting parameters")
+console.log(`listenPort: ${listenPort}`)
+console.log(`consortiumId: ${consortiumId}`)
+console.log(`containerName: ${containerName}`)
+console.log(`identityBlobPrefix: ${identityBlobPrefix}`)
+console.log(`ethRpcPort: ${ethRpcPort}`)
+console.log(`validatorListBlobName: ${validatorListBlobName}`)
+console.log(`paritySpecBlobName: ${paritySpecBlobName}`)
+console.log(`valSetContractBlobName: ${valSetContractBlobName}`)
+console.log(`adminContractBlobName: ${adminContractBlobName}`)
+console.log(`adminContractABIBlobName: ${adminContractABIBlobName}`)
+console.log(`Started EtherAdmin website - Ver.${appjson.version}`);
+
+
+function getRecentBlock() {
+  return new Promise(function (resolve, reject) {
+    try {
+      var web3RPC = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:' + ethRpcPort));
+    } catch (err) {
+      console.log(err);
+    }
+    var latestBlockNumber = web3RPC.eth.blockNumber;
+    var recentBlockNumber = Math.max(latestBlockNumber - recentBlockDecrement, 1);
+
+    web3RPC.eth.getBlock(recentBlockNumber, function (error, result) {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject('Unable to get a recent block');
+      }
+    })
+  });
+}
+
+
+/* 
+ * Given a node hostinfo object, collect node information (Consortium Id, PeerCount, Latest Block #) 
+ */
+function getNodeInfo(hostinfo, ipAddress) {
+  return new Promise(function (resolve, reject) {
+    try {
+      var web3RPC = new Web3(new Web3.providers.HttpProvider('http://' + ipAddress + ':' + ethRpcPort));
+    } catch (err) {
+      console.log(err);
+    }
+    var web3PromiseArray = [];
+    web3PromiseArray.push(new Promise(function (resolve, reject) {
+      web3RPC.net.getPeerCount(function (error, result) {
+        if (!error) {
+          resolve(result);
+        } else {
+          resolve('Not running');
+        }
+      })
+    }));
+    web3PromiseArray.push(new Promise(function (resolve, reject) {
+      web3RPC.eth.getBlockNumber(function (error, result) {
+        if (!error) {
+          resolve(result);
+        } else {
+          resolve('Not running');
+        }
+      })
+    }));
+    Promise.all(web3PromiseArray).then(function (values) {
+      var peerCount = values[0];
+      var blockNumber = values[1];
+      var nodeInfo = {
+        hostname: hostinfo.hostname,
+        peercount: peerCount,
+        blocknumber: blockNumber,
+        consortiumid: consortiumId,
+        enodeUrl: hostinfo.enodeUrl
+      }
+      resolve(nodeInfo);
+    });
+  });
+} 
+
+function getAddressListExists() {
+  return new Promise((resolve, reject) => {
+    blobService.doesBlobExist(containerName, validatorListBlobName, function (err, result) {
+      if (err) {
+        console.log(`Error trying to determine if ${validatorListBlobName} exists in ${containerName}`);
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(result.exists);
+      }
+    })
+  })
+}
+
+function getAddressListContents() {
+  return new Promise((resolve, reject) => {
+    getAddressListExists()
+      .then(function (exists) {
+        if (exists) {
+          console.log(`${validatorListBlobName} file exists.`);
+
+          var addressListPromise = new Promise((resolve, reject) => {
+            blobService.getBlobToText(
+              containerName,
+              validatorListBlobName,
+              function (err, blobText, blockBlob) {
+                if (err) {
+                  reject(err);
+                } else {
+                  console.log(`${validatorListBlobName} contents: ${blobText}`)
+                  var addressListObject = JSON.parse(blobText);
+                  resolve(addressListObject)
+                }
+              }
+            )
+          });
+          addressListPromise.then((result) => {
+            resolve(result);
+          });
+        } else {
+          resolve(null);
+        }
+      });
+  });
+}
+
+function getBlobs(continuationToken = null) {
+  var promise = new Promise((resolve, reject) => {
+    resolve(["passphrase-0.json","passphrase-1.json", "passphrase-2.json", "passphrase-3.json"]);
+  });
+  return promise;
+}
+
+function getLeasedBlobList(listBlobs) {
+  var promise = new Promise((resolve, reject) => {
+    var x = 100;
+    resolve([
+      {
+        name: 'passphrase-0.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-1.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-2.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-3.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-4.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-5.json',
+        state: 'leased'
+      },
+      {
+        name: 'passphrase-6.json',
+        state: 'leased'
+      }
+    ]);
+  });
+  return promise;
+}
+
+function getAbiDatafromBlob() {
+  var abiPromise = new Promise((resolve, reject) => {
+    var contract = require("../build/contracts/AdminValidatorSet.json");
+    var simpleContract = require("../build/contracts/SimpleValidatorSet.json");
+    var addresses = require("../keystore/contractsConfig.json");
+    resolve(JSON.stringify({contract, simpleContract, 
+      adminValidatorSetAddress: addresses.adminValidatorSetAddress,
+      simpleValidatorSetAddress: addresses.simpleValidatorSetAddress }));
+  });
+  abiPromise.then(function (contents) {
+    contents = JSON.parse(contents);
+    abiContent = contents.contract.abi;
+    simpleContent = contents.simpleContract.abi;
+    adminValidatorSetAddress = contents.adminValidatorSetAddress;
+    simpleValidatorSetAddress = contents.simpleValidatorSetAddress;
+  });
+}
+
+function getActiveNodeDetails(leasedList) {
+  var nodePromiseArray = [];
+  var promise = new Promise((resolve, reject) => {
+    if (leasedList.length == 0) {
+      // No list       
+      resolve([]);
+    }
+
+    leasedList.forEach((value) => {
+
+      var filecontent = require("./config/"+value.name);
+      var result = filecontent.enodeUrl.match(nodeRegexExp);
+
+      var promise = new Promise(function (resolve, reject) {
+        resolve(getNodeInfo(filecontent, "localhost"));
+      });
+
+      nodePromiseArray.push(promise);
+    });
+
+    Promise.all(nodePromiseArray).then(function (values) {
+      if (values.length == 0) {
+        resolve("No Values");
+      }
+      timeStamp = moment().format('h:mm:ss A UTC,  MMM Do YYYY');
+      var resultSet = values.sort();
+      resolve(resultSet);
+    });
+  });
+
+  return promise;
+  }
+
+function getNodesfromBlob() {
+  // Get Node info
+  getBlobs()
+    .then(getLeasedBlobList)
+    .then(getActiveNodeDetails).catch(function (error) {
+      console.log(`Error occurs while getting node details : ${error}`);
+    })
+    .then(function (activeNodesList) {
+      activeNodes = activeNodesList;
+    });
+}
+
+console.log('Start EtherAdmin Site');
+setInterval(getNodesfromBlob, refreshInterval);
+getAbiDatafromBlob();
+// getAddressListContents().then((result) => {
+//   if (result) {
+//     console.log(`getAddressListContents() returns: ${JSON.stringify(result)}`);
+//     addressList = result;
+//   } else {
+//     console.log(`${validatorListBlobName} does not exist`);
+//   }
+// });
+
+app.get('/', function (req, res) {
+  var hasNodeRows = activeNodes.length > 0;
+  if (hasNodeRows) {
+    var data = {
+      hasNodeRows: hasNodeRows,
+      consortiumid: consortiumId,
+      nodeRows: activeNodes,
+      timestamp: timeStamp,
+      refreshinterval: (refreshInterval / 1000),
+      contractAbi: abiContent,
+      nodes: {
+        adminContractAbi: JSON.stringify(abiContent),
+        adminContractAddress: adminValidatorSetAddress,
+        simpleContractAbi: JSON.stringify(simpleContent),
+        simpleContractAddress: simpleValidatorSetAddress
+      }
+    };
+    res.render('etheradmin', data);
+  } else {
+    res.render('etherstartup');
+  }
+});
+
+// Get:networkinfo
+app.get('/networkinfo', function (req, res) {
+  var networkInfo = new NetworkInfo();
+  networkInfo.adminContractABI = abiContent;
+  if (addressList)
+    networkInfo.addressList = addressList;
+  // Get Node info
+  getBlobs()
+    .then(getLeasedBlobList)
+    .then(getActiveNodeDetails).catch(function (error) {
+      console.log(`Error occurs while getting node details : ${error}`);
+      networkInfo.errorMessage += error + "\n";
+    })
+    .then(function (activeNodesList) {
+      if (activeNodesList.length > 0) {
+        activeNodesList.forEach((nodeInfo) => {
+          networkInfo.bootnodes.push(nodeInfo.enodeUrl);
+        });
+
+      } else {
+        networkInfo.errorMessage += "Couldn't find any active nodes\n";
+      }
+    })
+    .then(getRecentBlock)
+    .then(function (recentBlock) {
+      // Get paritySpecBlobName
+      networkInfo.recentBlock = recentBlock;
+      networkInfo.paritySpec = '{ "params": {"networkID":"2018"} }';
+
+      networkInfo.adminContract = fs.readFileSync("../contracts//AdminValidatorSet.sol")
+      networkInfo.valSetContract = fs.readFileSync("../contracts/SimpleValidatorSet.sol");
+      res.send(JSON.stringify(networkInfo));
+    })
+})
+
+// Used for sharing information about the network to joining members
+function NetworkInfo() {
+  // Indicates break in compatibility
+  this.majorVersion = 0;
+  // Indicates backward compatible change
+  this.minorVersion = 0;
+  this.bootnodes = [];
+  this.valSetContract = "";
+  this.adminContract = "";
+  this.adminContractABI = "";
+  this.paritySpec = "";
+  this.errorMessage = "";
+  this.recentBlock = "";
+}
+
+app.get('/AdminValidatorSet.js', function (req, res) {
+  var file = __dirname.replace("/app", "") + '/adminvalidatorset.js';
+  res.download(file);
+});
+
+app.get('/SimpleValidatorSet.js', function (req, res) {
+  var file = __dirname.replace("/app", "") + '/simplevalidatorset.js';
+  res.download(file);
+});
+
+app.get('/ethereumjs-tx-1.3.3.min.js', function (req, res) {
+  var file = __dirname.replace("/app", "/app/dist") + '/ethereumjs-tx-1.3.3.min.js';
+  res.download(file);
+});
+
+app.get('/web3util.js', function (req, res) {
+  var file = __dirname + '/web3util.js';
+  res.download(file);
+});
+
+app.get('/web3.1-beta.js', function (req, res) {
+  var file = __dirname.replace("/app", "/app/dist") + '/web3.min.js';
+  res.download(file);
+});
+
+app.listen(listenPort, function () {
+  console.log('Admin webserver listening on port ' + listenPort);
+});
+
---
+++ app/web3util.js
@@ -0,0 +1,208 @@
+
+class Utils {
+    constructor(web3provider, utils) {
+    }
+
+    async transaction (from,to,value,data){
+        return {
+            from    : from,
+            to      : to,
+            data    : data,
+            value   : value,
+            gasPrice: '0x00',
+            gas     : 4700000
+        }
+    }
+
+    async getContractEncodeABI(abi,bytecode,web3,arg){
+        try{
+            let contract = new web3.eth.Contract(JSON.parse(abi));
+            return await contract.deploy({ data : bytecode, arguments : arg }).encodeABI();
+        } catch (error) {
+            console.log("Exception in utils.getContractEncodeABI(): " + error);
+        } 
+    }
+    
+    async deployContract(contractAbi, bytecode, deployedAddress, constructorParameters, web3 /*callback*/) {
+        console.log("deployContract");
+        try{
+            let deployedContract = new web3.eth.Contract(JSON.parse(contractAbi));
+            deployedAddress = await deployedContract.deploy({
+                data : bytecode, 
+                arguments: constructorParameters
+            })
+            .send({
+                from : deployedAddress,
+                gas : 5500000
+            });
+            // .on('error', function(error){ 
+            //     if(!error)
+            //         console.log("error", error);
+            // })
+            // .on('receipt', receipt => {
+            //     deployedAddress = receipt.contractAddress;
+            //     // implement it on asysn await.
+            //     return deployedAddress;
+            // })
+            // .on('transactionHash', function(transactionHash){
+            //     console.log('transactionHash', transactionHash);
+            //     //web3.eth.getTransaction(transactionHash);
+            //     callback("transactionHash", transactionHash);
+            // })
+            // .then(transaction => {
+            //     console.log("transaction",transaction);
+            // });
+            return deployedAddress._address;
+        } catch (error) {
+            console.log("Exception in utils.deployContract(): " + error);
+        }    
+    }
+    
+    sendMethodTransaction (fromAccountAddress, toContractAddress, methodData, privateKey, web3, estimatedGas, fn)
+    {//, calleeMethodName,callback) {
+        web3.eth.getTransactionCount(fromAccountAddress, 'pending', function(err,  nonceToUse) {
+            console.log("nonceToUse ",nonceToUse);
+            const txParams = {
+                nonce: nonceToUse,
+                gasPrice: '0x00',
+                gasLimit: 4700000, //estimatedGas, //20000000, // Todo, estimate gas
+                from: fromAccountAddress,
+                to: toContractAddress,
+                value: '0x00',
+                data: methodData
+            }
+            const tx = new ethereumjs.Tx(txParams)
+            //const privateKeyBuffer = new ethereumjs.Buffer.Buffer.from(privateKey, 'hex');
+            //tx.sign(privateKeyBuffer);
+            const serializedTx = tx.serialize();
+
+            //web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            web3.eth.sendTransaction(txParams)
+            .on('receipt', function(r, e) {
+                fn(null, r);
+            })
+            .on('error', function(e, r) {
+                fn(e, null);
+            })
+        });
+   }
+    
+    /** To get estimate of gas consumptio for the given transaction prior to actual
+     * execution on blockchain! Extremely useful feature however, giving issues on quorum
+    */
+   async estimateGasTransaction (fromAccountAddress, toContractAddress, methodData, web3) {
+        return await web3.eth.estimateGas(
+            {
+                from    : fromAccountAddress,
+                to      : toContractAddress,
+                data    : methodData
+            });
+    }
+
+    /** to get receipt of the event raised from the blockchain
+    */ 
+    async getReceipt(transactionHash,web3){
+        var receipt = web3.eth.getTransactionReceipt(transactionHash);
+        if(!receipt)
+            console.log("Transaction",transactionHash,"did not get mined!");
+        return receipt;
+    }
+    
+    // readSolidityContractJSON (filename) {
+    //     var json = JSON.parse(fs.readFileSync(filename, 'utf8'));
+    //     let abi = JSON.stringify(json.abi);
+    //     return [abi, json.bytecode];
+    // },
+
+    getABIBytecodeAdmin(filename,contractName) {
+        // let source = fs.readFileSync(filename, 'utf8');
+        // let compiledContract = solc.compile(source, 1);
+        // let abi = compiledContract.contracts[":"+contractName].interface;
+        // let bytecode = compiledContract.contracts[":"+contractName].bytecode;
+
+        let abi = [{"inputs":[{"name":"owner1","type":"address"},{"name":"owner2","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"vfor","type":"address"}],"name":"votedfor","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"vfor","type":"address"}],"name":"votedagainst","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_address","type":"address"}],"name":"ownerAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_address","type":"address"}],"name":"ownerRemoved","type":"event"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"proposalToRemoveAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"proposalToAddAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"voteForAddingAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"voteForRemovingAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"voteAgainstAddingAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"voteAgainstRemovingAdmin","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"changeVote","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"checkAdmin","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getAllAdmins","outputs":[{"name":"res","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"checkVotes","outputs":[{"name":"res","type":"uint32[2]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"checkProposal","outputs":[{"name":"res","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"getVoted","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getTotalCount","outputs":[{"name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}];
+        let bytecode = "0x60806029";
+        
+
+        return [abi, bytecode];
+    }
+
+    getABIBytecodeSimple(filename,contractName) {
+        
+        let abi = [{"inputs":[{"name":"_address","type":"address"},{"name":"_validator1","type":"address"},{"name":"_validator2","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"validator","type":"address"},{"indexed":false,"name":"_admin","type":"address"}],"name":"addvalidator","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"validator","type":"address"},{"indexed":false,"name":"_admin","type":"address"}],"name":"removevalidator","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"validator","type":"address"},{"indexed":false,"name":"_admin","type":"address"},{"indexed":false,"name":"_event","type":"string"}],"name":"finalizeEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"vfor","type":"address"}],"name":"votedfor","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"vfor","type":"address"}],"name":"votedagainst","type":"event"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"addValidator","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"removeValidator","outputs":[{"name":"res","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"checkProposal","outputs":[{"name":"res","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getValidatorsForAdmin","outputs":[{"name":"a","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"isValidator","outputs":[{"name":"a","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getAllValidators","outputs":[{"name":"a","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"}];
+        let bytecode = "0x608f0029";
+
+        return [abi, bytecode];
+    }
+
+    keccak (web3,text){
+        return web3.utils.keccak256(text);
+    }
+
+    async sendTransaction(web3,transaction){
+        return await web3.eth.sendTransaction(transaction);
+    }
+
+    generatePublicKey (privateKey) {
+        return '0x'+ethUtil.privateToAddress(privateKey).toString('hex');
+    }
+
+    getPrivateKeyFromKeyStore (accountAddress, keyStorePath, password) {
+        var keyObject = keythereum.importFromFile(accountAddress, keyStorePath);
+        var privateKey = keythereum.recover(password, keyObject);
+        return privateKey.toString('hex');
+    }
+
+    async subscribe (string,web3,callback) {
+        web3.eth.subscribe(string,(error,transaction)=>{
+            if(error){
+                console.log("error",`SUBSCRIBE:\n${error.message}\n${error.stack}`);
+            }else{
+                callback(transaction);
+            }
+        });
+    }
+    
+    // to get all events from a submitted transaction to send to node application
+    async listen(contract,callback){
+        contract.events.allEvents({
+            fromBlock: 0,
+            toBlock  : 'latest'
+        },(err,event)=>{
+            if(err){
+                console.log('error',`\n${err.message}\n${err.stack}`)
+            }else{
+                console.log('info',`:\n${event}`);
+                callback(event);
+            }
+        });
+    }
+
+    async getData(fromAccount,toContract,endata,web3){
+        return await web3.eth.call({
+            from : fromAccount,
+            to: toContract,
+            data: endata
+        });
+    }
+
+    split(array){
+        var temp = [];
+        var add = [];
+        array = array.slice(2,array.length);
+        for(var i=0;i<array.length;i+=64){
+            temp.push(array.slice(i,i+64));
+        }
+        for(var j=0;j<temp.length;j++){
+            add.push("0x"+temp[j].slice(24,64));
+        }
+        return add.splice(2, add.length);
+    }
+
+    convertToBool(inputString){
+        if(inputString == "0x0000000000000000000000000000000000000000000000000000000000000001")
+            return true;
+        else (inputString == "0x0000000000000000000000000000000000000000000000000000000000000000")
+            return false;
+    }
+}
